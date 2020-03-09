6726dc1fb414b6d8d0f8ab54ac63cd06084df1f2
Matthieu Dorier <mdorier@anl.gov>
2020-03-09 19:01:04 +0000

    added --only dependencies

--- ci/jenkins-theta-build.sh
+++ ci/jenkins-theta-build.sh
@@ -14,19 +14,19 @@ echo "Loading Spack"
 
 if [[ $BUILD_TYPE = "release" ]]
 then
-    spack install mochi-all
+    spack install --only dependencies mochi-all
     if [[ $ENABLE_PYTHON = "python" ]]
     then
         echo "Python is enabled"
-        spack install py-mochi-all
+        spack install --only dependencies py-mochi-all
     fi
 elif [[ $BUILD_TYPE = "develop" ]]
 then
-    spack install mochi-all@develop
+    spack install --only dependencies mochi-all@develop
     if [[ $ENABLE_PYTHON = "python" ]]
     then
         echo "Python is enabled"
-        spack install py-mochi-all@develop
+        spack install --only dependencies py-mochi-all@develop
     fi
 else
     echo "Invalid build type (should be release of develop)"
