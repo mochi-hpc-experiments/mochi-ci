53077e866a463db962a8c9c4a0e66885f9e4e622
Matthieu Dorier <mdorier@anl.gov>
2020-03-09 10:56:44 +0000

    added python

--- ci/jenkins-mcs-workstation-build.sh
+++ ci/jenkins-mcs-workstation-build.sh
@@ -3,16 +3,31 @@ set -e
 
 HERE=`dirname "$0"`
 
+BUILD_TYPE=${1:-release}
+ENABLE_PYTHON=${2:-none}
+
 echo "Loading Spack"
 . spack/share/spack/setup-env.sh
 
 echo "Building all Mochi components (${BUILD_TYPE})"
 if [[ $BUILD_TYPE = "release" ]]
 then
-    spack install mochi-all
+    if [[ $ENABLE_PYTHON = "python" ]]
+    then
+        echo "Python is enabled"
+        spack install py-mochi-all
+    else
+        spack install mochi-all
+    fi
 elif [[ $BUILD_TYPE = "develop" ]]
 then
-    spack install mochi-all@develop
+    if [[ $ENABLE_PYTHON = "python" ]]
+    then
+        echo "Python is enabled"
+        spack install py-mochi-all@develop
+    else
+        spack install mochi-all@develop
+    fi
 else
     echo "Invalid build type (should be release of develop)"
     exit 1
--- ci/jenkins-theta-build.sh
+++ ci/jenkins-theta-build.sh
@@ -12,13 +12,24 @@ module load cce
 echo "Loading Spack"
 . spack/share/spack/setup-env.sh
 
-echo "Building all Mochi components (${BUILD_TYPE})"
 if [[ $BUILD_TYPE = "release" ]]
 then
-    spack install mochi-all
+    if [[ $ENABLE_PYTHON = "python" ]]
+    then
+        echo "Python is enabled"
+        spack install py-mochi-all
+    else
+        spack install mochi-all
+    fi
 elif [[ $BUILD_TYPE = "develop" ]]
 then
-    spack install mochi-all@develop
+    if [[ $ENABLE_PYTHON = "python" ]]
+    then
+        echo "Python is enabled"
+        spack install py-mochi-all@develop
+    else
+        spack install mochi-all@develop
+    fi
 else
     echo "Invalid build type (should be release of develop)"
     exit 1
