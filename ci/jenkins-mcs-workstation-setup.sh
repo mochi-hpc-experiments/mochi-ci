9ef67ee29782f6e03114a7b032d6ae890ea4979c
Matthieu Dorier <mdorier@anl.gov>
2020-06-08 13:31:17 +0100

    ensured spack doesn't use /tmp to build stuff

--- ci/jenkins-mcs-workstation-setup.sh
+++ ci/jenkins-mcs-workstation-setup.sh
@@ -6,11 +6,11 @@ HERE=`dirname "$0"`
 echo "Setting up Spack"
 git clone https://github.com/spack/spack.git
 . spack/share/spack/setup-env.sh
-#spack bootstrap
 
 echo "Copying packages.yaml file"
 mkdir -p $HOME/.spack/linux
 cp $HERE/mcs-workstation-packages.yaml $HOME/.spack/linux/packages.yaml
+cp $HERE/mcs-workstation-config.yaml $HOME/.spack/config.yaml
 
 echo "Setting up sds-repo"
 git clone https://xgitlab.cels.anl.gov/sds/sds-repo.git
