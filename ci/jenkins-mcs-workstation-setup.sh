7f0e8cdeca2954f1d6e6acbc67eae3cd52394200
Matthieu Dorier <mdorier@anl.gov>
2021-02-17 13:41:13 +0000

    another attempt at fixing the pipeline

--- ci/jenkins-mcs-workstation-setup.sh
+++ ci/jenkins-mcs-workstation-setup.sh
@@ -12,9 +12,6 @@ mkdir -p $HOME/.spack/linux
 cp $HERE/mcs-workstation-packages.yaml $HOME/.spack/linux/packages.yaml
 cp $HERE/mcs-workstation-config.yaml $HOME/.spack/config.yaml
 
-echo "Discovering system packages"
-spack external find
-
 echo "Setting up sds-repo"
 git clone https://xgitlab.cels.anl.gov/sds/sds-repo.git
 spack repo add sds-repo
