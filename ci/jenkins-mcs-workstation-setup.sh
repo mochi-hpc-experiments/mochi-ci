b39a00eb0c5b81f052da9fed2762af9c9e003906
Matthieu Dorier <mdorier@anl.gov>
2021-03-22 16:40:09 +0000

    updated link to mochi spack packages

--- ci/jenkins-mcs-workstation-setup.sh
+++ ci/jenkins-mcs-workstation-setup.sh
@@ -12,6 +12,6 @@ mkdir -p $HOME/.spack/linux
 cp $HERE/mcs-workstation-packages.yaml $HOME/.spack/linux/packages.yaml
 cp $HERE/mcs-workstation-config.yaml $HOME/.spack/config.yaml
 
-echo "Setting up sds-repo"
-git clone https://xgitlab.cels.anl.gov/sds/sds-repo.git
-spack repo add sds-repo
+echo "Setting up Mochi spack packages"
+git clone https://github.com/mochi-hpc/mochi-spack-packages.git
+spack repo add mochi-spack-packages
