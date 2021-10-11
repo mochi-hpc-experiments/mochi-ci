56df24d012299c6b74aedcd0a76a4f66f7ded4a7
Matthieu Dorier <mdorier@anl.gov>
2021-10-11 19:35:58 +0000

    re-enabling sdskv

--- ci/jenkins-mcs-workstation-build.sh
+++ ci/jenkins-mcs-workstation-build.sh
@@ -67,7 +67,7 @@ do
   try_build mochi-ssg          $BUILD_TYPE $LOGFILE
   try_build mochi-colza        $BUILD_TYPE $LOGFILE
   try_build mochi-bake         $BUILD_TYPE $LOGFILE
-#  try_build mochi-sdskv        $BUILD_TYPE $LOGFILE
+  try_build mochi-sdskv        $BUILD_TYPE $LOGFILE
   try_build mochi-remi         $BUILD_TYPE $LOGFILE
   try_build mochi-poesie       $BUILD_TYPE $LOGFILE
   try_build mochi-sonata       $BUILD_TYPE $LOGFILE
@@ -77,7 +77,7 @@ do
   try_build py-mochi-ssg       $BUILD_TYPE $LOGFILE
   try_build py-mochi-remi      $BUILD_TYPE $LOGFILE
   try_build mochi-yokan        $BUILD_TYPE $LOGFILE
-#  try_build py-mochi-sdskv     $BUILD_TYPE $LOGFILE
+  try_build py-mochi-sdskv     $BUILD_TYPE $LOGFILE
   try_build mobject            $BUILD_TYPE $LOGFILE
 #  try_build mochi-sdsdkv       $BUILD_TYPE $LOGFILE
 #  prepare_python
