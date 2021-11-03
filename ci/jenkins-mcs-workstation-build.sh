9a1bb393102edd9c015b03e8da9e17098c5443a0
Matthieu Dorier <mdorier@anl.gov>
2021-11-03 05:04:11 -0500

    adding build with clang

--- ci/jenkins-mcs-workstation-build.sh
+++ ci/jenkins-mcs-workstation-build.sh
@@ -5,10 +5,11 @@ HERE=`dirname "$0"`
 STATUS=0
 
 function try_build {
-  PACKAGE=$1
-  SPACKENV=$1-env
-  VERSION=$2
-  LOGFILE=$3
+  COMPILER=$1
+  PACKAGE=$2
+  SPACKENV=$2-env
+  VERSION=$3
+  LOGFILE=$4
   DEVELOP=false
   echo "#######################################################"
   echo "#######################################################"
@@ -25,18 +26,18 @@ function try_build {
   spack env create $SPACKENV ci/mcs-workstation-env.yaml
   spack env activate $SPACKENV
   if $DEVELOP ; then
-    spack add $PACKAGE@develop
+    spack add $PACKAGE@develop %$COMPILER
     spack install
     RET=$?
   else
-    spack add $PACKAGE
+    spack add $PACKAGE %$COMPILER
     spack install
     RET=$?
   fi
   if [[ "$RET" = "0" ]]; then
-    printf "%-40s => OK\n" "$PACKAGE ($VERSION)" >> $LOGFILE
+    printf "%-40s => OK\n" "$PACKAGE ($VERSION - $COMPILER)" >> $LOGFILE
   else
-    printf "%-40s => FAILURE\n" "$PACKAGE ($VERSION)" >> $LOGFILE
+    printf "%-40s => FAILURE\n" "$PACKAGE ($VERSION - $COMPILER)" >> $LOGFILE
     STATUS=1
   fi
   spack env deactivate
@@ -54,35 +55,38 @@ echo "Loading Spack"
 
 LOGFILE=build.txt
 
+for COMPILER in "gcc@9.1.0" "clang@8.0.0"
+do
 for BUILD_TYPE in release develop
 do
 
   echo "========== ${BUILD_TYPE} =========" >> $LOGFILE
   echo "Building all Mochi components (${BUILD_TYPE})"
-  try_build mochi-mona         $BUILD_TYPE $LOGFILE
-  try_build mochi-abt-io       $BUILD_TYPE $LOGFILE
-  try_build mochi-margo        $BUILD_TYPE $LOGFILE
-  try_build mochi-ch-placement $BUILD_TYPE $LOGFILE
-  try_build mochi-thallium     $BUILD_TYPE $LOGFILE
-  try_build mochi-ssg          $BUILD_TYPE $LOGFILE
-  try_build mochi-colza        $BUILD_TYPE $LOGFILE
-  try_build mochi-bake         $BUILD_TYPE $LOGFILE
-  try_build mochi-sdskv        $BUILD_TYPE $LOGFILE
-  try_build mochi-remi         $BUILD_TYPE $LOGFILE
-  try_build mochi-poesie       $BUILD_TYPE $LOGFILE
-  try_build mochi-sonata       $BUILD_TYPE $LOGFILE
-  try_build mochi-bedrock      $BUILD_TYPE $LOGFILE
-  try_build py-mochi-margo     $BUILD_TYPE $LOGFILE
-  try_build py-mochi-bake      $BUILD_TYPE $LOGFILE
-  try_build py-mochi-ssg       $BUILD_TYPE $LOGFILE
-  try_build py-mochi-remi      $BUILD_TYPE $LOGFILE
-  try_build mochi-yokan        $BUILD_TYPE $LOGFILE
-  try_build py-mochi-sdskv     $BUILD_TYPE $LOGFILE
-  try_build mobject            $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-mona         $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-abt-io       $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-margo        $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-ch-placement $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-thallium     $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-ssg          $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-colza        $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-bake         $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-sdskv        $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-remi         $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-poesie       $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-sonata       $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-bedrock      $BUILD_TYPE $LOGFILE
+  try_build $COMPILER py-mochi-margo     $BUILD_TYPE $LOGFILE
+  try_build $COMPILER py-mochi-bake      $BUILD_TYPE $LOGFILE
+  try_build $COMPILER py-mochi-ssg       $BUILD_TYPE $LOGFILE
+  try_build $COMPILER py-mochi-remi      $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mochi-yokan        $BUILD_TYPE $LOGFILE
+  try_build $COMPILER py-mochi-sdskv     $BUILD_TYPE $LOGFILE
+  try_build $COMPILER mobject            $BUILD_TYPE $LOGFILE
 #  try_build mochi-sdsdkv       $BUILD_TYPE $LOGFILE
 #  prepare_python
 #  try_build py-mochi-tmci      $BUILD_TYPE $LOGFILE
 
+done
 done
 
 cat $LOGFILE
